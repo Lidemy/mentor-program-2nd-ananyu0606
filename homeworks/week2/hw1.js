c5aec28083f9cdd67956032a341adb91c29ea116
Ann <ananyu0606@gmail.com>
2018-09-12 10:59:51 +0800

    week2_hw1.js_done

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,15 @@
-function stars(n) {
+// 給定 n（1<=n<=30），依照規律「回傳」正確圖形（每一行是一個陣列的元素）
+// n = 1 回傳["*"]
+// n = 3 回傳["*", "**", "***"]
 
+function stars(n) {
+    var arr = []
+    for (var i = 1; i <= n; i++){
+        arr.push( '*'.repeat(i))
+    }
+    return arr
 }
 
 module.exports = stars;
+// console.log(stars(1))
+// console.log(stars(3))
